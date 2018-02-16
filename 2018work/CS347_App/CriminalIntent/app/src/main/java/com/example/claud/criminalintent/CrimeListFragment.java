cb34ec2f5458e4aad64fe21e85282e7820674df9
Claudiu Moise <claudiu.moise@gmail.com>
2018-02-15 21:00:29 -0600

    Made Start menu + changed colors

---
+++ 2018work/CS347_App/CriminalIntent/app/src/main/java/com/example/claud/criminalintent/CrimeListFragment.java
@@ -0,0 +1,70 @@
+package com.example.claud.criminalintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.List;
+
+/**
+ * Created by claud on 2/15/2018.
+ */
+
+public class CrimeListFragment extends Fragment{
+    private RecyclerView mCrimeRecyclerView;
+    private CrimeAdapter mAdapter;
+
+    private class CrimeHolder extends RecyclerView.ViewHolder{
+
+        public CrimeHolder(LayoutInflater inflater, ViewGroup parent){
+            super(inflater.inflate(R.layout.list_item_crime, parent, false));
+        }
+    }
+
+    private class CrimeAdapter extends RecyclerView.Adapter<CrimeHolder>{
+        private List<Crime> mCrimes;
+
+        public CrimeAdapter(List<Crime> crimes){
+            mCrimes = crimes;
+        }
+
+        @Override
+        public CrimeHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            return new CrimeHolder(layoutInflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(CrimeHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return mCrimes.size();
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_crime_list, container, false);
+        mCrimeRecyclerView= v.findViewById(R.id.crime_recycler_view);
+
+        //
+        mCrimeRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        CrimeLab crimeLab = CrimeLab.get(getActivity());
+        List<Crime> crimes = crimeLab.getCrimes();
+        mAdapter= new CrimeAdapter(crimes);
+        mCrimeRecyclerView.setAdapter(mAdapter);
+
+        return v;
+    }
+}
---
+++ 2018work/CS347_App/gildasclub/app/src/main/java/com/example/claud/gildasclub/Locations.java
@@ -0,0 +1,8 @@
+package com.example.claud.gildasclub;
+
+/**
+ * Created by claud on 2/15/2018.
+ */
+
+public class Locations {
+}
