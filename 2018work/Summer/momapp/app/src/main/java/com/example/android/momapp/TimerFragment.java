d3fb514ff556d8641057e056b2add19a1ea1c510
Claudiu Moise <claudiu.moise@gmail.com>
2018-07-12 18:43:13 -0500

    Momapp Timer

--- 2018work/Summer/momapp/app/src/main/java/com/example/android/momapp/TimerFragment.java
+++ 2018work/Summer/momapp/app/src/main/java/com/example/android/momapp/TimerFragment.java
@@ -5,6 +5,7 @@ import android.os.CountDownTimer;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,7 +20,8 @@ import static java.lang.Integer.parseInt;
 public class TimerFragment extends Fragment{
 
     private static final long START_TIME_IN_MILLIS = 600000;
-    private EditText mUserTimerInputField;
+    private EditText mSecondsEditText;
+    private EditText mMinutesEditText;
     private Button mUserTimerInput;
 
 
@@ -28,27 +30,61 @@ public class TimerFragment extends Fragment{
     private Button mTimerReset;
 
     private boolean mTimerRunning;
-    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private static long mTimeLeftInMillis = START_TIME_IN_MILLIS;
 
     private CountDownTimer mCountdownTimer;
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            //Restore the fragment's state here
+            Log.d(TAG, "Claude Lifecycle: fragment state here");
+            System.out.println("SOPL fragment's state here");
+            //mTimeLeftInMillis = savedInstanceState.get();
+
+            Fragment mContent = getFragmentManager().getFragment(savedInstanceState, "TimerFragment");
+        }
+
         //good practice, allows for referencing
         View v = inflater.inflate(R.layout.fr_timer, container, false);
 
         mTextViewCountdownTimer = (TextView) v.findViewById(R.id.tv_timer);
-        mUserTimerInputField = (EditText) v.findViewById(R.id.et_timer_entry);
+        mSecondsEditText = (EditText) v.findViewById(R.id.et_timer_entry_s);
+        mMinutesEditText = (EditText) v.findViewById(R.id.et_timer_entry_m);
+
 
         mUserTimerInput = (Button) v.findViewById(R.id.bt_timer_input);
         mUserTimerInput.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String woah = mUserTimerInputField.getText().toString();
-                int dude = parseInt(woah);
-                long userInputInMillis = dude*1000;
-                mTimeLeftInMillis = userInputInMillis;
+                long inputSecondsInMillis;
+                long inputMinutesInMillis;
+
+                if(mSecondsEditText.getText().toString().equals("")){
+                    inputSecondsInMillis = 0;
+                }
+                else{
+                    String woah = mSecondsEditText.getText().toString();
+                    int dude = parseInt(woah);
+                    inputSecondsInMillis = dude*1000;
+                }
+
+                if(mMinutesEditText.getText().toString().equals("")){
+                    inputMinutesInMillis = 0;
+                }
+                else {
+                    String woah2 = mMinutesEditText.getText().toString();
+                    int dude2 = parseInt(woah2);
+                    inputMinutesInMillis = dude2*60000;
+                }
+
+                mTimeLeftInMillis = inputSecondsInMillis+inputMinutesInMillis;
+
+                startTimer();
             }
         });
 
@@ -64,6 +100,8 @@ public class TimerFragment extends Fragment{
 
             }
         }
+
+
 
         );
 
@@ -126,6 +164,57 @@ public class TimerFragment extends Fragment{
         String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d", minutes, seconds);
 
         mTextViewCountdownTimer.setText(timeLeftFormatted);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "Claude Lifecycle: onStart");
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "Claude Lifecycle: onStart");
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "Claude Lifecycle: onResume");
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "Claude Lifecycle: onPause");
+
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "Claude Lifecycle: onStop");
+//        onSaveInstanceState();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "Claude Lifecycle: onDestroy");
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        //reffering to THIS fragment
+        getFragmentManager().putFragment(outState, "TimerFragment", this);
+
+        //String lifecycleDisplayTextViewContents = mLifecycleDisplay.getText().toString();
+        //outState.putString(LIFECYCLE_CALLBACKS_TEXT_KEY, lifecycleDisplayTextViewContents);
     }
 
 }
