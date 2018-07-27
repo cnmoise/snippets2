c4581e6f213e3693c418e9ec8094435d2dadaa1f
Claudiu Moise <claudiu.moise@gmail.com>
2018-07-27 18:14:52 -0500

    Implementing Dialog Functionality2

---
+++ 2018work/Summer/Android/dialogtest/app/src/main/java/com/example/claud/dialogtesting/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.claud.dialogtesting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements ExampleDialog.ExampleDialogListener {
+
+    private TextView textViewUsername;
+    private TextView textViewPassword;
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textViewUsername = (TextView) findViewById(R.id.textview_username);
+        textViewPassword = (TextView) findViewById(R.id.textview_password);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+
+    }
+
+    public  void openDialog(){
+        ExampleDialog exampleDialog = new ExampleDialog();
+        exampleDialog.show(getSupportFragmentManager(), "example dialog");
+    }
+
+    @Override
+    public void applyTexts(String username, String password) {
+//        textViewUsername.setText(username);
+//        textViewPassword.setText(password);
+        Log.d("claude", "Behold Dialog" + username);
+        Log.d("claude", "Behold Dialog" + password);
+    }
+}
