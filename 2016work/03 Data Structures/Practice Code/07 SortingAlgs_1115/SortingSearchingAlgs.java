5fca6ca1911c980b4abc3dcccc9098bc36d4d76f
cnmoise <claudiu.moise@gmail.com>
2018-09-12 07:47:04 -0500

    Updates

--- 2016work/03 Data Structures/Practice Code/07 SortingAlgs_1115/SortingSearchingAlgs.java
+++ 2016work/03 Data Structures/Practice Code/07 SortingAlgs_1115/SortingSearchingAlgs.java
@@ -22,6 +22,9 @@ public class SortingSearchingAlgs
         quickSort(testArray);
         System.out.println("The array sorted by Quick Sort is " + Arrays.toString(testArray));
         
+        testArray = myArray.clone();
+        QuickSortV2(testArray, 0, testArray.length - 1);
+        System.out.println("The array sorted by QuickSortV2 is " + Arrays.toString(testArray));
     }
     
     // find the index of the smallest element in the speficied range
@@ -189,4 +192,51 @@ public class SortingSearchingAlgs
     {
         quickSortRec(numArray, 0, numArray.length - 1);
     }
+    
+    
+    public static int partition(int arr[], int low, int high) 
+    { 
+        int pivot = arr[high];  
+        int i = (low-1); // index of smaller element 
+        for (int j=low; j<high; j++) 
+        { 
+            // If current element is smaller than or 
+            // equal to pivot 
+            if (arr[j] <= pivot) 
+            { 
+                i++; 
+  
+                // swap arr[i] and arr[j] 
+                int temp = arr[i]; 
+                arr[i] = arr[j]; 
+                arr[j] = temp; 
+            } 
+        } 
+  
+        // swap arr[i+1] and arr[high] (or pivot) 
+        int temp = arr[i+1]; 
+        arr[i+1] = arr[high]; 
+        arr[high] = temp; 
+  
+        return i+1; 
+    }
+    
+    /* The main function that implements QuickSort() 
+      arr[] --> Array to be sorted, 
+      low  --> Starting index, 
+      high  --> Ending index */
+    public static  void QuickSortV2(int arr[], int low, int high) 
+    { 
+        if (low < high) 
+        { 
+            /* pi is partitioning index, arr[pi] is  
+              now at right place */
+            int pi = partition(arr, low, high); 
+  
+            // Recursively sort elements before 
+            // partition and after partition 
+            QuickSortV2(arr, low, pi-1); 
+            QuickSortV2(arr, pi+1, high); 
+        } 
+    } 
 }
