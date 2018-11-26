660f415447b39477e8a75b1706f96406b0436abb
cnmoise <claudiu.moise@gmail.com>
2018-11-25 18:19:30 -0600

    Google Practice

---
+++ 01_Java/Java_Practice/HackerRank Java/2018.11.24_(Arrays)NewYearsChaos.java
@@ -0,0 +1,14 @@
+//https://www.hackerrank.com/challenges/new-year-chaos
+
+static void minimumBribes(int[] q) {
+        int ans = 0;
+        for (int i = q.length-1; i >= 0; i--) {
+            if (q[i] - (i + 1) > 2) {
+                System.out.println("Too chaotic");
+                return;
+            }
+            //count the bribes
+            for (int j = Math.max(0, q[i] - 2); j < i; j++)
+                if (q[j] > q[i]) ans++;
+        }
+        System.out.println(ans);     
